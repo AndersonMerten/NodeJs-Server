12675daba5c683f030ebd00de5af058fd96de740
andersonmerten <anderson.merten@gmail.com>
2018-08-22 23:56:26 -0300

    fix data troubles

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var json = require('./clientes.json');
+
+for(cliente in json){
+  console.log("Cliente");
+  console.log(cliente);
+}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/dados',(req,res) => res.render('pages/db'))
+  .get('/dados',(req,res) => res.render('pages/db', {data: json}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
